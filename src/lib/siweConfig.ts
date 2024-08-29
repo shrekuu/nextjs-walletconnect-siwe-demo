a2fd0aef50f2c882f596dd7c6bbb2e350f5e2977
Bond <shrekuu@gmail.com>
2024-08-29 10:12:27 +0800

    feat: add siwe signOutOnAccountChange=false signOutOnNetworkChange=false

--- src/lib/siweConfig.ts
+++ src/lib/siweConfig.ts
@@ -1,57 +1,59 @@
-import { getCsrfToken, signIn, signOut, getSession } from 'next-auth/react'
-import type { SIWEVerifyMessageArgs, SIWECreateMessageArgs, SIWESession } from '@web3modal/siwe'
-import { createSIWEConfig, formatMessage } from '@web3modal/siwe'
-import { mainnet, sepolia } from 'viem/chains'
+import { getCsrfToken, signIn, signOut, getSession } from "next-auth/react";
+import type { SIWEVerifyMessageArgs, SIWECreateMessageArgs, SIWESession } from "@web3modal/siwe";
+import { createSIWEConfig, formatMessage } from "@web3modal/siwe";
+import { mainnet, sepolia } from "viem/chains";
 
 export const siweConfig = createSIWEConfig({
+  signOutOnAccountChange: false,
+  signOutOnNetworkChange: false,
   getMessageParams: async () => ({
-    domain: typeof window !== 'undefined' ? window.location.host : '',
-    uri: typeof window !== 'undefined' ? window.location.origin : '',
+    domain: typeof window !== "undefined" ? window.location.host : "",
+    uri: typeof window !== "undefined" ? window.location.origin : "",
     chains: [mainnet.id, sepolia.id],
-    statement: 'Please sign with your account'
+    statement: "Please sign with your account",
   }),
   createMessage: ({ address, ...args }: SIWECreateMessageArgs) => formatMessage(args, address),
   getNonce: async () => {
-    const nonce = await getCsrfToken()
+    const nonce = await getCsrfToken();
     if (!nonce) {
-      throw new Error('Failed to get nonce!')
+      throw new Error("Failed to get nonce!");
     }
 
-    return nonce
+    return nonce;
   },
   getSession: async () => {
-    const session = await getSession()
+    const session = await getSession();
     if (!session) {
-      throw new Error('Failed to get session!')
+      throw new Error("Failed to get session!");
     }
 
-    const { address, chainId } = session as unknown as SIWESession
+    const { address, chainId } = session as unknown as SIWESession;
 
-    return { address, chainId }
+    return { address, chainId };
   },
   verifyMessage: async ({ message, signature }: SIWEVerifyMessageArgs) => {
     try {
-      const success = await signIn('credentials', {
+      const success = await signIn("credentials", {
         message,
         redirect: false,
         signature,
-        callbackUrl: '/protected'
-      })
+        callbackUrl: "/protected",
+      });
 
-      return Boolean(success?.ok)
+      return Boolean(success?.ok);
     } catch (error) {
-      return false
+      return false;
     }
   },
   signOut: async () => {
     try {
       await signOut({
-        redirect: false
-      })
+        redirect: false,
+      });
 
-      return true
+      return true;
     } catch (error) {
-      return false
+      return false;
     }
-  }
-})
+  },
+});
