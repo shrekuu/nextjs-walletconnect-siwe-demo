36a423550689a6800f787928d9b6f21a50e55175
Bond <shrekuu@gmail.com>
2024-08-29 15:36:12 +0800

    fix: build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.externals = [...config.externals, "pino", "pino-pretty", "jsdom"];
+
+    // Important: return the modified config
+    return config;
+  },
+};
 
 export default nextConfig;
