fda0c544af01291bb6e156cad79cc2c5df4e1431
Bond <shrekuu@gmail.com>
2024-09-02 11:16:54 +0800

    feat: test custom next auth url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,27 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import AppWagmiProvider from "@/lib/AppWagmiProvider";
+import { SessionProvider } from "next-auth/react";
+import { getServerSession } from "next-auth";
 
 export const metadata: Metadata = {
   title: "Next.js WalletConnect SIWE Demo",
   description: "WalletConnect Web3Modal + SIWE + Next.js + NextAuth.js",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await getServerSession();
+
   return (
     <html lang="en">
       <body>
-        <AppWagmiProvider>{children}</AppWagmiProvider>
+        <SessionProvider session={session} basePath="/next-api/api/auth">
+          <AppWagmiProvider>{children}</AppWagmiProvider>
+        </SessionProvider>
       </body>
     </html>
   );
